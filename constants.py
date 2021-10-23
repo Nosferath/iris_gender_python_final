a5ae5f79f875676be8d8b8230fc34c4667ed6491
Claudio Yanez <claudio.yanez@ing.uchile.cl>
2021-10-23 12:56:13 -0300

    Prepared main.py file for easier testing

--- constants.py
+++ constants.py
@@ -1,8 +1,12 @@
 datasets = ('left_240x20_fixed', 'right_240x20_fixed',
             'left_240x40_fixed', 'right_240x40_fixed',
-            'left_480x80_fixed', 'right_480x80_fixed',
-            'left_240x20', 'right_240x20', 'left_240x40',
-            'right_240x40', 'left_480x80', 'right_480x80')
+            'left_240x20', 'right_240x20',
+            'left_240x40', 'right_240x40')
+# datasets = ('left_240x20_fixed', 'right_240x20_fixed',
+#             'left_240x40_fixed', 'right_240x40_fixed',
+#             'left_240x20', 'right_240x20', 'left_240x40',
+#             'right_240x40', 'left_480x80_fixed', 'right_480x80_fixed',
+#             'left_480x80', 'right_480x80')
 MASK_VALUE = 0
 TEST_PROPORTION = 0.4
 SCALE_DATASET = True
