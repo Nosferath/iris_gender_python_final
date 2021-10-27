1f0a479c847ded50728bf355221983a30c2c506b
Claudio Yanez <claudio.yanez@ing.uchile.cl>
2021-10-27 12:35:45 -0300

    Readied main for regular cmim tests

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ def perform_regular_tests(n_jobs: int):
         t(n_jobs=n_jobs)
 
 
+def perform_regular_cmim_tests(n_jobs: int):
+    from ada_test import main_cmim as main_ada
+    from ada_test import main_std_cmim as main_std_ada
+    from bagging_test import main_cmim as main_bag
+    from bagging_test import main_std_cmim as main_std_bag
+    from rf_test import main_cmim as main_rf
+    from rf_test import main_std_cmim as main_std_rf
+    tests = [main_ada, main_std_ada, main_bag, main_std_bag, main_rf,
+             main_std_rf]
+    for t in tests:
+        for n_cmim in (2, 4):
+            t(n_cmim=n_cmim, n_jobs=n_jobs)
+
+
 def perform_mod_v2_cmim_tests(n_jobs: int):
     from ada_test import main_cmim_mod_v2 as main_ada
     from ada_test import main_std_cmim_mod_v2 as main_std_ada
