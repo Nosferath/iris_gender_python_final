0f08a0fbf9a3fbc7ae46ef8c3226b9ea6ccae05b
Claudio Yanez <claudio.yanez@ing.uchile.cl>
2021-11-12 18:33:50 -0300

    Implemented plotting inverted masks

--- plots_tesis.py
+++ plots_tesis.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -41,70 +42,44 @@ def calculate_mask_percentage(dataset_name: str, use_pairs: bool,
     return masks
 
 
-def generate_mask_hists(dataset_name: str, use_pairs: bool, max_y: int = None):
+def generate_mask_hists(dataset_name: str, use_pairs: bool, max_y: int = None,
+                        out_folder: str = r'S:\Tesis\Figuras\HistosFull'):
     """Generates a histogram describing the percentage of mask present
     in every image of the dataset.
     """
     # Obtain mask percentages
     masks = calculate_mask_percentage(dataset_name, use_pairs, genders=False)
     # Define histogram bins
-    bins = np.linspace(0, 100, 11)
+    bins = np.linspace(0, 100, 21)
     # Generate histogram
+    sns.set_theme()
     sns.set_context("talk")
     fig, ax = plt.subplots()
     hist = sns.histplot(masks, bins=bins, ax=ax)
     title = f'Máscaras en dataset {dataset_name}'
-    if use_pairs:
-        title += '\ntras emparejar'
-    else:
-        title = '\n' + title
+    out_name = dataset_name
+    if dataset_name.endswith('_fixed'):
+        title = title[:-6] + ' tras corrección'
+    # if use_pairs:
+    #     title += '\ntras emparejar'
+    #     out_name += '_pairs'
+    # else:
+    #     title = '\n' + title
+    out_name += '.png'
     ax.set_title(title)
     if max_y is not None:
         ax.set_ylim([0, max_y])
+    plt.xlim([0, 80])
+    plt.xlabel('% de máscara en la imagen')
+    plt.ylabel('No. de imágenes')
     plt.tight_layout()
-    plt.show()
+    # Save figure
+    out_folder = Path(out_folder)
+    out_folder.mkdir(exist_ok=True, parents=True)
+    plt.savefig(out_folder / out_name)
 
 
-def generate_mask_hists_by_gender(dataset_name: str, use_pairs: bool,
-                                  max_y: int = None):
-    """Generates a histogram describing the percentage of mask present
-    in every image of the dataset, separated by gender.
-    """
-    # Obtain mask percentages
-    masks_f, masks_m = calculate_mask_percentage(dataset_name, use_pairs,
-                                                 genders=True)
-    # Define histogram bins
-    bins = np.linspace(0, 100, 11)
-    # Generate histogram
-    sns.set_theme()
-    sns.set_context("paper")
-    fig, axs = plt.subplots(2, 1)
-    hist_f = sns.histplot(masks_f, bins=bins, ax=axs[0], color='red',
-                          label='Mujeres')
-    hist_m = sns.histplot(masks_m, bins=bins, ax=axs[1], color='blue',
-                          label='Hombres')
-    # Generate and set title
-    title = f'Máscaras por género en dataset {dataset_name}'
-    if use_pairs:
-        title += ' tras emparejar'
-    fig.suptitle(title)
-    # Generate and set legend
-    # handles_labels = [ax.get_legend_handles_labels() for ax in axs]
-    # handles, labels = [sum(ll, []) for ll in zip(*handles_labels)]
-    # leg = plt.legend(handles, labels)
-    # leg.set_in_layout(True)
-    if max_y is None:
-        max_f = np.max(np.histogram(masks_f, bins)[0])
-        max_m = np.max(np.histogram(masks_m, bins)[0])
-        max_y = int(1.05 * max(max_m, max_f))
-    for ax in axs:
-        ax.set_ylim([0, max_y])
-        ax.legend()
-    plt.tight_layout()
-    plt.show()
-
-
-def generate_mask_hists_by_gender2(
+def generate_mask_hists_by_gender(
         dataset_name: str, use_pairs: bool, max_y: int = None,
         out_folder: str = r'S:\Tesis\Figuras\Histogramas'):
     """Generates a histogram describing the percentage of mask present
@@ -124,10 +99,15 @@ def generate_mask_hists_by_gender2(
                           label='Hombres', alpha=0.5)
     # Generate and set title
     title = f'Máscaras por género en dataset {dataset_name}'
-    out_name = dataset_name
-    if use_pairs:
+    out_name = 'g' + dataset_name
+    if use_pairs and dataset_name.endswith('_fixed'):
+        title = title[:-6] + '\ncorregidas, tras emparejar'
+        out_name += '_pairs'
+    elif use_pairs:
         title += '\ntras emparejar'
         out_name += '_pairs'
+    elif dataset_name.endswith('_fixed'):
+        title = title[:-6] + '\ntras corrección'
     else:
         title = '\n' + title
     out_name += '.png'
@@ -149,7 +129,8 @@ def generate_mask_hists_by_gender2(
     plt.clf()
 
 
-def generate_masked_iris_image(x_arr: np.ndarray, m_arr: np.ndarray):
+def generate_masked_iris_image(x_arr: np.ndarray, m_arr: np.ndarray,
+                               color: str = 'G', invert: bool = False):
     """Reshapes the x_arr to its final shape, and applies the mask in
     green for visualization."""
     shapes = {4800: (20, 240), 9600: (40, 240), 38400: (80, 480)}
@@ -159,16 +140,21 @@ def generate_masked_iris_image(x_arr: np.ndarray, m_arr: np.ndarray):
     x_arr = x_arr.reshape(shape).astype('uint8')
     m_arr = m_arr.reshape(shape)
     # Extend x to 3-D
+    colors = {'R': (255, 0, 0), 'G': (0, 255, 0), 'B': (0, 0, 255)}
     x_arr = np.tile(x_arr[..., np.newaxis], (1, 1, 3))
-    x_arr[m_arr == 1] = np.array((0, 255, 0), dtype='uint8')
+    if not invert:
+        x_arr[m_arr == 1] = np.array(colors[color], dtype='uint8')
+    else:
+        x_arr[m_arr == 0] = np.array(colors[color], dtype='uint8')
     return x_arr
 
 
-def visualize_pairs(dataset_name: str, pair_idx: int = 0,
+def visualize_pairs(dataset_name: str, pair: Tuple[int],
                     out_folder: str = r'S:\Tesis\Figuras\Pares'):
     """Visualizes iris before and after pairing."""
+    # TODO implementar pairs_idx como argumento excluyente con pair
     from PIL import Image
-    from pairs import load_pairs_array
+    # from pairs import load_pairs_array
     # Load dataset and pairs
     train_x, _, train_m, _, _, _, _, _ = load_partitions_cmim(
         dataset_name=dataset_name,
@@ -178,25 +164,104 @@ def visualize_pairs(dataset_name: str, pair_idx: int = 0,
         pair_method=False,
         n_cmim=0
     )
-    pairs = load_pairs_array(dataset_name, PAIR_METHOD, partition=1)
+    # pairs = load_pairs_array(dataset_name, PAIR_METHOD, partition=1)
     # Select pair and extract
-    cur_pair = pairs[pair_idx, :]
-    x_a = train_x[int(cur_pair[0]), :]
-    m_a = train_m[int(cur_pair[0]), :]
-    x_b = train_x[int(cur_pair[1]), :]
-    m_b = train_m[int(cur_pair[1]), :]
+    # pair = pairs[pair_idx, :]
+    x_a = train_x[int(pair[0]), :]
+    m_a = train_m[int(pair[0]), :]
+    x_b = train_x[int(pair[1]), :]
+    m_b = train_m[int(pair[1]), :]
     # Generate visualizations
-    x_a_unpaired = generate_masked_iris_image(x_a, m_a)
-    x_b_unpaired = generate_masked_iris_image(x_b, m_b)
+    x_a_unpaired = generate_masked_iris_image(x_a, m_a, color='B')
+    x_b_unpaired = generate_masked_iris_image(x_b, m_b, color='R')
     m_pair = np.any(np.vstack([m_a, m_b]), axis=0)
-    x_a_paired = generate_masked_iris_image(x_a, m_pair)
-    x_b_paired = generate_masked_iris_image(x_b, m_pair)
+    x_a_paired = generate_masked_iris_image(x_a, m_pair, color='G')
+    x_b_paired = generate_masked_iris_image(x_b, m_pair, color='G')
     # Save visualizations
     out_folder = Path(out_folder)
     out_folder.mkdir(exist_ok=True, parents=True)
     out_names = ['a_unpaired.png', 'b_unpaired.png',
                  'a_paired.png', 'b_paired.png']
     out_arrs = [x_a_unpaired, x_b_unpaired, x_a_paired, x_b_paired]
+    for out_name, out_arr in zip(out_names, out_arrs):
+        img = Image.fromarray(out_arr)
+        img.save(out_folder / out_name)
+    # Generate mask visualization
+    shapes = {4800: (20, 240), 9600: (40, 240), 38400: (80, 480)}
+    shape = shapes[x_a.size]
+    shape3 = (*shape, 3)
+    m_a_vis = np.zeros(shape3, dtype='uint8')
+    m_a_vis[m_a.reshape(shape) == 1, 2] = 255
+    m_b_vis = np.zeros(shape3, dtype='uint8')
+    m_b_vis[m_b.reshape(shape) == 1, 0] = 255
+    m_ab_vis1 = np.stack([m_b_vis[..., 0],
+                          np.zeros(shape, dtype='uint8'),
+                          m_a_vis[..., 2]], axis=-1)
+    m_ab_vis2 = np.zeros(shape3, dtype='uint8')
+    m_ab_vis2[m_pair.reshape(shape) == 1, 1] = 255
+    out_names = ['ma.png', 'mb.png',
+                 'mab1.png', 'mab2.png']
+    out_arrs = [m_a_vis, m_b_vis, m_ab_vis1, m_ab_vis2]
+    for out_name, out_arr in zip(out_names, out_arrs):
+        img = Image.fromarray(out_arr)
+        img.save(out_folder / out_name)
+
+
+def visualize_inv_masks(dataset_name: str, pair: Tuple[int],
+                        out_folder: str = r'S:\Tesis\Figuras\Invertidas'):
+    """Visualizes iris before and after pairing."""
+    # TODO refactor
+    from PIL import Image
+    # from pairs import load_pairs_array
+    # Load dataset and pairs
+    train_x, _, train_m, _, _, _, _, _ = load_partitions_cmim(
+        dataset_name=dataset_name,
+        partition=1,  # Not relevant
+        mask_value=0,  # Not relevant
+        scale_dataset=True,  # Not relevant
+        pair_method=False,
+        n_cmim=0
+    )
+    # pairs = load_pairs_array(dataset_name, PAIR_METHOD, partition=1)
+    # Select pair and extract
+    # pair = pairs[pair_idx, :]
+    x_a = train_x[int(pair[0]), :]
+    m_a = train_m[int(pair[0]), :]
+    x_b = train_x[int(pair[1]), :]
+    m_b = train_m[int(pair[1]), :]
+    # Generate visualizations
+    x_a_unpaired = generate_masked_iris_image(x_a, m_a, color='B', invert=True)
+    x_b_unpaired = generate_masked_iris_image(x_b, m_b, color='R', invert=True)
+    m_pair = np.any(np.vstack([(m_a != 1), (m_b != 1)]), axis=0) != 1
+    x_a_paired = generate_masked_iris_image(x_a, m_pair, color='G',
+                                            invert=True)
+    x_b_paired = generate_masked_iris_image(x_b, m_pair, color='G',
+                                            invert=True)
+    # Save visualizations
+    out_folder = Path(out_folder)
+    out_folder.mkdir(exist_ok=True, parents=True)
+    out_names = ['a_unpaired.png', 'b_unpaired.png',
+                 'a_paired.png', 'b_paired.png']
+    out_arrs = [x_a_unpaired, x_b_unpaired, x_a_paired, x_b_paired]
+    for out_name, out_arr in zip(out_names, out_arrs):
+        img = Image.fromarray(out_arr)
+        img.save(out_folder / out_name)
+    # Generate mask visualization
+    shapes = {4800: (20, 240), 9600: (40, 240), 38400: (80, 480)}
+    shape = shapes[x_a.size]
+    shape3 = (*shape, 3)
+    m_a_vis = np.zeros(shape3, dtype='uint8')
+    m_a_vis[m_a.reshape(shape) == 0, 2] = 255
+    m_b_vis = np.zeros(shape3, dtype='uint8')
+    m_b_vis[m_b.reshape(shape) == 0, 0] = 255
+    m_ab_vis1 = np.stack([m_b_vis[..., 0],
+                          np.zeros(shape, dtype='uint8'),
+                          m_a_vis[..., 2]], axis=-1)
+    m_ab_vis2 = np.zeros(shape3, dtype='uint8')
+    m_ab_vis2[m_pair.reshape(shape) == 0, 1] = 255
+    out_names = ['ma.png', 'mb.png',
+                 'mab1.png', 'mab2.png']
+    out_arrs = [m_a_vis, m_b_vis, m_ab_vis1, m_ab_vis2]
     for out_name, out_arr in zip(out_names, out_arrs):
         img = Image.fromarray(out_arr)
         img.save(out_folder / out_name)
