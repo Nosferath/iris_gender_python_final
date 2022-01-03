21adf65a139d9da37916a965beea631c9e503304
Claudio Yanez <claudio.yanez@ing.uchile.cl>
2022-01-03 16:40:14 -0300

    Implemented VGG feats LSVM test

--- vgg.py
+++ vgg.py
@@ -20,30 +20,21 @@ def load_vgg_model_features():
     return model
 
 
-def prepare_data_for_vgg(train_x: np.ndarray, train_y: np.ndarray,
-                         test_x: np.ndarray, test_y: np.ndarray):
+def prepare_data_for_vgg(data_x: np.ndarray):
     from skimage.color import gray2rgb
     from skimage.transform import resize
     from tensorflow.keras.applications.vgg16 import preprocess_input
     from utils import find_shape
 
-    orig_shape = find_shape(n_features=train_x.shape[1])
-    train_x = gray2rgb(train_x.reshape((-1, *orig_shape)))
-    test_x = gray2rgb(test_x.reshape((-1, *orig_shape)))
-    out_train_x = np.zeros((train_x.shape[0], 224, 224, 3))
-    for i in range(train_x.shape[0]):
-        cur_img = train_x[i]
+    orig_shape = find_shape(n_features=data_x.shape[1])
+    data_x = gray2rgb(data_x.reshape((-1, *orig_shape)))
+    out_data_x = np.zeros((data_x.shape[0], 224, 224, 3))
+    for i in range(data_x.shape[0]):
+        cur_img = data_x[i]
         cur_img = resize(cur_img, output_shape=(224, 224), mode='edge',
                          order=3)
-        out_train_x[i, :, :, :] = cur_img
-    out_test_x = np.zeros((test_x.shape[0], 224, 224, 3))
-    for i in range(test_x.shape[0]):
-        cur_img = test_x[i]
-        cur_img = resize(cur_img, output_shape=(224, 224), mode='edge',
-                         order=3)
-        out_test_x[i, :, :, :] = cur_img
+        out_data_x[i, :, :, :] = cur_img
 
-    out_train_x = preprocess_input(out_train_x)
-    out_test_x = preprocess_input(out_test_x)
+    out_data_x = preprocess_input(out_data_x)
 
-    return out_train_x, train_y, out_test_x, test_y
+    return out_data_x
---
+++ vgg_iris_gender.py
@@ -0,0 +1,86 @@
+import pickle
+from multiprocessing import Pool
+from pathlib import Path
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import LinearSVC
+
+from constants import TEST_SIZE
+from load_data import load_iris_dataset
+from load_data_utils import partition_data
+from utils import Timer
+from vgg import load_vgg_model_features, prepare_data_for_vgg
+
+
+def vgg_feat_lsvm_parall(data_x, labels, partition: int):
+    # Prepare data
+    train_x, train_y, test_x, test_y = partition_data(
+        data_x, labels, TEST_SIZE, partition
+    )
+    # Train model
+    model = Pipeline([
+        ('scaler', MinMaxScaler()),
+        ('model', LinearSVC(max_iter=5000,
+                            random_state=42))
+    ])
+    model.fit(train_x, train_y)
+    pred = model.predict(test_x)
+    results = classification_report(test_y, pred, output_dict=True)
+
+    del train_x, train_y, test_x, test_y
+    return results
+
+
+def perform_vgg_feat_lsvm_test(dataset_name: str, n_partitions: int,
+                               n_jobs: int):
+    feat_model = load_vgg_model_features()
+    data, labels = load_iris_dataset(dataset_name, None)
+    data = prepare_data_for_vgg(data)
+    data = feat_model.predict(data)
+
+    args = [(data, labels, i) for i in range(n_partitions)]
+    with Pool(n_jobs) as p:
+        print("VGG Features, LSVM Test")
+        t = Timer(f"{dataset_name}, {n_partitions} partitions, {n_jobs} jobs")
+        t.start()
+        results = p.starmap(vgg_feat_lsvm_parall, args)
+        t.stop()
+
+    out_folder = Path('vgg_feat_lsvm_results')
+    out_folder.mkdir(exist_ok=True, parents=True)
+    with open(out_folder / f'{dataset_name}.pickle', 'wb') as f:
+        pickle.dump(results, f)
+
+    del data, labels
+
+
+def main_vgg_feat_lsvm_test():
+    import argparse
+    from constants import datasets
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument('n_jobs', type=int,
+                    help='Number of jobs')
+    ap.add_argument('-p', '--n_parts', type=int, default=10,
+                    help='Number of random partitions to test on')
+    args = ap.parse_args()
+    n_jobs = args.n_jobs
+    n_parts = args.n_parts
+
+    for d in datasets[1:]:  # TODO borrar []
+        perform_vgg_feat_lsvm_test(d, n_parts, n_jobs)
+
+
+# def perform_vgg_test(dataset_name: str, n_partitions: int,
+#                      n_jobs: int):
+
+
+def main_vgg_test():
+    pass
+
+
+if __name__ == '__main__':
+    main_vgg_feat_lsvm_test()
