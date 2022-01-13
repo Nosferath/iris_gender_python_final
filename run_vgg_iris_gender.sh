a690cde4c6e355c04e886fc50953afb9116070e0
Claudio Y <x_metalnos_x@hotmail.com>
2022-01-13 10:37:59 -0300

    Adding partial VGG full botheyes results

--- run_vgg_iris_gender.sh
+++ run_vgg_iris_gender.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 run_vgg_iris_gender(){
   conda activate iris_keras
-  for d in $(seq 0 1); do
-    for i in $(seq 1 $1); do
+  for i in $(seq 1 10); do
+    python ./vgg_iris_gender.py -d 0 -p $i --use_peri
+    for d in $(seq 0 3); do
       # echo "Testing this thing $i"
-      python ./vgg_iris_gender.py -d $d -p $i --use_peri
+      python ./vgg_iris_gender.py -d $d -p $i
+      python ./vgg_iris_gender.py -d $d -p $i --use_val
     done
+    python ./vgg_iris_gender.py -d 0 -p $i --use_peri --use_val
+  done
+  for i in $(seq 10 20); do
+    python ./vgg_iris_gender.py -d 0 -p $i --use_peri
+    for d in $(seq 0 3); do
+      # echo "Testing this thing $i"
+      python ./vgg_iris_gender.py -d $d -p $i
+      python ./vgg_iris_gender.py -d $d -p $i --use_val
+    done
+    python ./vgg_iris_gender.py -d 0 -p $i --use_peri --use_val
+  done
+  for i in $(seq 20 30); do
+    python ./vgg_iris_gender.py -d 0 -p $i --use_peri
+    for d in $(seq 0 3); do
+      # echo "Testing this thing $i"
+      python ./vgg_iris_gender.py -d $d -p $i
+      python ./vgg_iris_gender.py -d $d -p $i --use_val
+    done
+    python ./vgg_iris_gender.py -d 0 -p $i --use_peri --use_val
   done
 }
