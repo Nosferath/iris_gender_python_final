30f2d1edd6bfaa477f5317bc511a537ed1b6f8d0
Claudio Y <x_metalnos_x@hotmail.com>
2022-02-11 18:10:29 -0300

    Adding vgg full 4 longer results

--- run_vgg_full.sh
+++ run_vgg_full.sh
@@ -3,12 +3,10 @@ run_vgg_full(){
     conda activate iris_keras
 
     for nPart in $(seq 1 1); do  # CHANGE NUMBERS APPROPRIATELY
-        # for d in $(seq 0 3); do
+        for d in $(seq 0 3); do
             # python ./vgg_full.py -d $d -p $i -e $2 -bs $1 -o step_by_step_vgg -lr 0.0005
-            # # # python ./vgg_full.py -pf step_by_step_vgg_1_lesslr/params.json -d $d -p $nPart -sbs
-            python ./vgg_full.py -pf step_by_step_vgg_1_lesslr/params.json -d 1 -p "$nPart" -sbs
-            python ./vgg_full.py -pf step_by_step_vgg_1_lesslr/params.json -d 3 -p "$nPart" -sbs
-        # done
+            python ./vgg_full.py -pf step_by_step_vgg_4_longer/params.json -d $d -p "$nPart" -sbs
+        done
         # python ./vgg_full.py -d 0 -p $i -e $2 --use_peri -bs $3 -o second_both_peri_vgg_results -lr 0.0005
         # python ./vgg_full.py -d 0 -p $i -e $2 --use_val --use_peri -bs $3 -o step_by_step_vgg_peri -lr 0.0005 -sbs
     done
