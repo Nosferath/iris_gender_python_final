c2b3be8ed2401bca22a972b824ef3865ac5e48b8
Claudio Yanez <claudio.yanez@ing.uchile.cl>
2022-02-14 13:59:21 -0300

    Processed tests 1 lrl and 4 longer, added test 4 longer 2

--- run_vgg_full.sh
+++ run_vgg_full.sh
@@ -5,7 +5,7 @@ run_vgg_full(){
     for nPart in $(seq 1 1); do  # CHANGE NUMBERS APPROPRIATELY
         for d in $(seq 0 3); do
             # python ./vgg_full.py -d $d -p $i -e $2 -bs $1 -o step_by_step_vgg -lr 0.0005
-            python ./vgg_full.py -pf step_by_step_vgg_4_longer/params.json -d $d -p "$nPart" -sbs
+            python ./vgg_full.py -pf step_by_step_vgg_4_longer2/params.json -d "$d" -p "$nPart" -sbs
         done
         # python ./vgg_full.py -d 0 -p $i -e $2 --use_peri -bs $3 -o second_both_peri_vgg_results -lr 0.0005
         # python ./vgg_full.py -d 0 -p $i -e $2 --use_val --use_peri -bs $3 -o step_by_step_vgg_peri -lr 0.0005 -sbs
