c69e28f0a2e03e76b0e21f6323ed4a9e256fc1bb
Claudio Y <x_metalnos_x@hotmail.com>
2022-03-04 17:51:51 -0300

    Adding sbs results for norm fix vgg full, and prepared full norm fix vgg full

--- run_vgg_full.sh
+++ run_vgg_full.sh
@@ -2,9 +2,9 @@
 run_vgg_full(){
     conda activate iris_keras
 
-    for nPart in $(seq 1 1); do  # CHANGE NUMBERS APPROPRIATELY
+    for nPart in $(seq 1 30); do  # CHANGE NUMBERS APPROPRIATELY
         for d in $(seq 0 3); do
-            python ./vgg_full.py -pf experiments/full_vgg/normalized_postfix_step_by_step/params.json -d "$d" -p "$nPart" -sbs
+            python ./vgg_full.py -pf experiments/full_vgg/normalized_postfix_full/params.json -d "$d" -p "$nPart" 
         done
         # python ./vgg_full.py -pf experiments/full_vgg/step_by_step_peri_fix_2_lesslr/params.json -d 0 -p "$nPart" --use_fix -sbs
     done
